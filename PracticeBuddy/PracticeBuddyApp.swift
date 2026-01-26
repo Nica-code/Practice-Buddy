7748d87d06d354faa76accbd3f286a9bae67a3ba
Alex Malaimare <contact@alexmalaimare.com>
2026-01-26 14:51:37 -0500

    Initial Commit

---
+++ PracticeBuddy/PracticeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeBuddyApp.swift
+//  PracticeBuddy
+//
+//  Created by Nica on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct PracticeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
